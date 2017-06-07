5f84566f09a13644de321a03ca48691184027f67
Ev Bogue <ev@evbogue.com>
2017-06-06 23:10:56 -0400

    load decent keys instead of ssb keys

--- client/keys.js
+++ client/keys.js
@@ -1,4 +1,4 @@
-var config = require('ssb-config/inject')(process.env.ssb_appname)
+var config = require('../plugins/ssb-config/inject')(process.env.ssb_appname)
 var ssbKeys      = require('ssb-keys')
 var path         = require('path')
 module.exports = ssbKeys.loadOrCreateSync(path.join(config.path, 'secret'))
