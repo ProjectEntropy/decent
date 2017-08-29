545f9935e262524d2df433a78f1c63aac5897341
Ev Bogue <ev@evbogue.com>
2017-08-29 11:15:04 -0700

    remove old client stuff

--- modules/index.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  "app.js":  require('./app.js'),
-  "avatar-profile.js": require('./avatar-profile.js'),
-  "tabs.js":  require('./tabs.js'),
-  "help.js": require('./help.js')
-}
-
