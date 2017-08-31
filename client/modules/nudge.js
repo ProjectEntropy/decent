bb52953ced7cc2e13a3abb5fd418bb0a99381bb1
Ev Bogue <ev@evbogue.com>
2017-08-31 13:46:41 -0700

    minor tweaks

--- client/modules/nudge.js
+++ client/modules/nudge.js
@@ -6,10 +6,10 @@ exports.needs = {
   avatar_name: 'first',
   avatar_link: 'first',
   message_confirm: 'first',
-  message_link: 'first',
 }
 
 exports.gives = {
+  message_content: true,
   message_content_mini: true,
   avatar_action: true,
 }
@@ -17,19 +17,21 @@ exports.gives = {
 exports.create = function (api) {
   var exports = {}
 
+  exports.message_content =
   exports.message_content_mini = function (msg, sbot) {
-    if(msg.value.content.type !== 'nudge') return
-    return h('span', 'nudged ', api.avatar_link(msg.value.content.id, api.avatar_name(msg.value.content.id)))
+    if(msg.value.content.type == 'nudge') {
+      return h('span', 'nudged ', api.avatar_link(msg.value.content.id, api.avatar_name(msg.value.content.id)))
+    }
   }
  
   exports.avatar_action = function (id) {
     return h('a', {href:'#', onclick: function () {
       api.message_confirm({
-	      type: 'nudge',
-	      id: id
+        type: 'nudge',
+        id: id
       })
-    }}, h('button', 'nudge'))
+    }}, h('button', 'Nudge'))
   }
 
-	return exports
+  return exports
 }
