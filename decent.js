78b06d2815f17e314befb90f7fc321573ba2781b
Ev Bogue <ev@evbogue.com>
2017-09-03 13:35:41 -0700

    put http server in decent.js

--- decent.js
+++ decent.js
@@ -1,4 +1,6 @@
 var fs = require('fs')
+var http = require('http')
+var serve = require('ecstatic')
 var path = require('path')
 var ssbKeys = require('ssb-keys')
 var stringify = require('pull-stringify')
@@ -14,15 +16,14 @@ var createSbot = require('./lib')
   .use(require('./plugins/replicate'))
   .use(require('ssb-friends'))
   .use(require('ssb-blobs'))
-  .use(require('./plugins/invite'))
+  //.use(require('./plugins/invite'))
   .use(require('./plugins/local'))
-  .use(require('./plugins/logging'))
+  //.use(require('./plugins/logging'))
   .use(require('./plugins/private'))
   .use(require('ssb-query'))
   .use(require('ssb-links'))
   .use(require('./plugins/ssb-ws'))
   .use(require('ssb-ebt'))
-  .use(require('./plugins/serve'))
 
 config.keys = keys
 var server = createSbot(config)
@@ -30,4 +31,8 @@ var server = createSbot(config)
 fs.writeFileSync(
   manifestFile, JSON.stringify(server.getManifest(), null, 2)
 )
+
+http.createServer(
+  serve({ root: __dirname + '/build'})
+).listen(3001)
 
