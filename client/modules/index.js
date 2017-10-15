0b8a3fb80af2ad7c140ea11209f7ab6e1906be1d
Ev Bogue <ev@evbogue.com>
2017-10-14 17:11:15 -0700

    remove channels from index

--- client/modules/index.js
+++ client/modules/index.js
@@ -34,7 +34,6 @@ module.exports = {
   "suggest-mentions.js":  require('./suggest-mentions.js'),
   "thread.js":  require('./thread.js'),
   "timestamp.js":  require('./timestamp.js'),
-  "channel.js":  require('./channel.js'),
   "emoji.js":  require('./emoji.js'),
   "nudge.js":  require('./nudge.js'),
   "suggest-emoji.js":  require('./suggest-emoji.js'),
