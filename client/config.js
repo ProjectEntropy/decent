3e5ea0d49eb27575e9c2baabc2ef94d4fdfb5775
Ev Bogue <ev@evbogue.com>
2017-10-15 11:45:35 -0700

    3.7.2 - set config.ws.remote as default localStorage.remote address

--- client/config.js
+++ client/config.js
@@ -3,6 +3,12 @@ var config = require('./../config/inject.js')(process.env.ssb_appname)
 var URL = require('url')
 
 module.exports = function () {
+
+  // set a default ws.remote if there is none, overrideable in the client
+  if ((localStorage.remote === undefined) || (localStorage.remote === '')) {
+    localStorage.remote = config.ws.remote
+  }
+  
   var remote = 'undefined' === typeof localStorage
     ? null //'ws://localhost:8989~shs:' + require('./keys')
     : localStorage.remote
