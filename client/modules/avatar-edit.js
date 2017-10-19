184972f9530c7fc4e687abbddedb23ae004e5d7d
Ev Bogue <ev@evbogue.com>
2017-10-19 13:47:51 -0700

    clean up avatar-edit

--- client/modules/avatar-edit.js
+++ client/modules/avatar-edit.js
@@ -5,26 +5,22 @@ var hypercrop = require('hypercrop')
 var hyperlightbox = require('hyperlightbox')
 var h = require('hyperscript')
 var pull = require('pull-stream')
-var getAvatar = require('ssb-avatar')
-var ref = require('ssb-ref')
-var visualize = require('visualize-buffer')
 var id = require('../keys').id
 
-function crop (d, cb) {
+function crop (data, cb) {
   var data
-  var canvas = hypercrop(h('img', {src: d}))
+
+  var canvas = hypercrop(h('img', {src: data}))
 
   return h('div.column.avatar--profile',
     canvas,
-    canvas.selection,
-    h('div.row.avatar_pic__controls',
-      h('button', 'okay', {onclick: function () {
-        cb(null, canvas.selection.toDataURL())
-      }}),
-      h('button', 'cancel', {onclick: function () {
-        cb(new Error('canceled'))
-      }})
-    )
+    //canvas.selection,
+    h('button.btn.btn-success', {onclick: function () {
+      cb(null, canvas.selection.toDataURL())
+    }}, 'Select'),
+    h('button.btn', {onclick: function () {
+      cb(new Error('canceled'), 'Cancel')
+    }})
   )
 }
 
@@ -33,7 +29,7 @@ exports.needs = {
   sbot_blobs_add: 'first',
   blob_url: 'first',
   sbot_links: 'first',
-  avatar_name: 'first'
+  avatar_image: 'first'
 }
 
 exports.gives = 'avatar_edit'
@@ -41,54 +37,16 @@ exports.gives = 'avatar_edit'
 exports.create = function (api) {
   return function () {
 
-    var img = visualize(new Buffer(id.substring(0), 'base64'), 256)
-    img.classList.add('avatar--profile')
+    var img = api.avatar_image(id, 'profile')
 
     var lb = hyperlightbox()
-    var name_input = h('input', {placeholder: 'rename'})
-    var name = api.avatar_name(id)
     var selected = null, selected_data = null
-
-    getAvatar({links: api.sbot_links}, id, id, function (err, avatar) {
-      if (err) return console.error(err)
-      //don't show user has already selected an avatar.
-      if(selected) return
-      if(ref.isBlob(avatar.image))
-        img.src = api.blob_url(avatar.image)
-    })
-
-    var also_pictured = h('div.profile__alsopicturedas.wrap')
-
-    pull(
-      api.sbot_links({dest: id, rel: 'about', values: true}),
-      pull.map(function (e) {
-        return e.value.content.image
-      }),
-      pull.filter(function (e) {
-        return e && 'string' == typeof e.link
-      }),
-      pull.unique('link'),
-      pull.drain(function (image) {
-        also_pictured.appendChild(
-          h('a', {href:'#', onclick: function (ev) {
-              ev.stopPropagation()
-              ev.preventDefault()
-              selected = image
-              img.src = api.blob_url(image.link || image)
-            }},
-            h('img.avatar--thumbnail', {src: api.blob_url(image)})
-          )
-        )
-      })
-    )
 
     return h('div.row.profile',
       lb,
       img,
       h('div.column.profile__info',
         h('strong', name),
-        name_input,
-
         hyperfile.asDataURL(function (data) {
           var el = crop(data, function (err, data) {
             if(data) {
@@ -108,7 +66,6 @@ exports.create = function (api) {
                     width: 512,
                     height: 512
                   }
-
                 })
               )
             }
@@ -117,9 +74,6 @@ exports.create = function (api) {
           lb.show(el)
         }),
         h('button', 'update', {onclick: function () {
-          if(name_input.value)
-            name.textContent = name_input.value
-
           if(selected)
             api.message_confirm({
               type: 'about',
@@ -127,17 +81,10 @@ exports.create = function (api) {
               name: name_input.value || undefined,
               image: selected
             })
-          else if(name_input.value) //name only
-            api.message_confirm({
-              type: 'about',
-              about: id,
-              name: name_input.value || undefined,
-            })
-          else
-            //another moment of weakness
+          else { 
             alert('must select a name or image')
-        }}),
-      also_pictured
+          }
+        }})
       )
     )
   }
